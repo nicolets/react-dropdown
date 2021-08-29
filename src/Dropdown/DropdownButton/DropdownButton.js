63745417ceb638a6426e2b0836e8de64c1e2d7da
Nicole <nicole30401@gmail.com>
2021-08-29 18:02:14 +0300

    bonus

--- src/Dropdown/DropdownButton/DropdownButton.js
+++ src/Dropdown/DropdownButton/DropdownButton.js
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ItemContext } from '../item-context';
 import './DropdownButton.scss';
 
 function DropdownButton(props) {
 
     const {shown, setShown} = useContext(ItemContext);
+    
+    useEffect(() => {
+        document.addEventListener('click', () => {
+            setShown(false);
+        });
+    }, []);
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+        setShown(!shown)
+    }
 
     return (
         <div>
-            <button onClick={() => setShown(!shown)}>Dropdown button</button>
+            <button onClick={handleClick}>Dropdown button</button>
         </div>
     );
 }
